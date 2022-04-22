af4cd1adbd527080b05d24b2a8385e3b3c65f6c5
Nuno <41781@ufp.edu.pt>
2022-04-22 15:41:43 +0100

    Added princeton library

--- src/com/projeto/DataBase.java
+++ src/com/projeto/DataBase.java
@@ -5,16 +5,50 @@ import edu.princeton.cs.algs4.*;
 import java.util.ArrayList;
 
 public class DataBase {
+    public BinarySearchST<Integer, Nodes> st = new BinarySearchST<Integer, Nodes>();
 
-
-    public BST<Integer, Nodes> addNode(int key, Nodes node) {
-
-        BST<Integer, Nodes> st = new BST<Integer, Nodes>();
-        st.put(key, node);
-        return st;
+    public DataBase() {
     }
 
+    /**
+     *  Inserts the specified key-value into the symbol table
+     * @param key - the key
+     * @param node - node class
+     */
+    public void addNode(int key, Nodes node) {
+        st.put(key, node);
+    }
 
+    /**
+     * Edits BST through key
+     * @param key - the key
+     * @param node - node class
+     **/
+    public void editNode(int key, Nodes node) {
+        if (st.get(key) != null) {
+            st.put(key, node);
+        }
+    }
+
+    /**
+     * Deletes key's node
+     * @param key - the key
+     */
+    public void deleteNode(int key){
+        st.delete(key);
+    }
+
+    /**
+     * Searches for the specified key's value
+     * @param key - the key
+     */
+    public Nodes searchNode(int key){
+        return st.get(key);
+    }
+
+    public void printBST(){
+        s
+    }
 
 
     public ArrayList whoVisited(Date d, PoI p) {
