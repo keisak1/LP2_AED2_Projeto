22bb5309118138215c142ad1a576a08a6a2ddac0
Nuno <41781@ufp.edu.pt>
2022-05-20 23:39:57 +0100

    ola

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,7 @@ module com.example.projeto_lp2_aed2_javafx {
     requires javafx.graphics;
     requires javafx.fxml;
     requires algs4.master.fc511547db;
-
+//ola
     opens com.projeto to javafx.fxml;
     exports com.projeto;
 }
