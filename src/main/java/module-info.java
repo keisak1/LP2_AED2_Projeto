91b8cb6c4b1e51b9b2d9c01a20011a2948c6d62a
Nuno <41781@ufp.edu.pt>
2022-05-20 23:50:01 +0100

    Maven implemented, GUI created

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,9 @@
-module com.example.projeto_lp2_aed2_javafx {
+module com.projeto {
     requires javafx.controls;
     requires javafx.graphics;
     requires javafx.fxml;
     requires algs4.master.fc511547db;
-//ola
+
     opens com.projeto to javafx.fxml;
     exports com.projeto;
 }
